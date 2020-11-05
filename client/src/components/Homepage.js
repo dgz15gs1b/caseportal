a987d02ca3faac5170bbce7527ca8d1910c4529e
Pengjie Zhou <zhoupj1215@hotmail.com>
2020-11-05 10:22:26 -0800

    finish table v1

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -15,6 +15,96 @@ import Paper from '@material-ui/core/Paper';
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 
+const rawData =
+    [
+      {
+        "id":"OHVB17801",
+        "name":"Katlyn Merriman",
+        "gender":"Female",
+        "age":"33",
+        "status history":{
+          "W":"2020/01/24 14:50:22",
+          "BAT":"2020/01/24 16:10:10",
+          "BAR":"2020/01/26 10:23:11",
+          "AMC":"2020/02/22 13:51:12"
+        },
+        "case status":"inactive",
+        "plan":"OHVB"
+      },
+      {
+        "id":"OHVB17802",
+        "name":"Gabriel King",
+        "gender":"Male",
+        "age":"54",
+        "status history":{
+          "W":"2020/02/01 10:11:21",
+          "BAT":"2020/02/01 15:02:58",
+          "BAR":"2020/02/04 11:11:11",
+          "DSS/G":"2020/02/15 09:20:22"
+        },
+        "case status":"inactive",
+        "plan":"OHVB"
+      },
+      {
+        "id":"OHVB17803",
+        "name":"Patricia Devries",
+        "gender":"Female",
+        "age":"62",
+        "status history":{
+          "W":"2020/08/17 16:17:22",
+          "BAT":"2020/08/19 15:45:22",
+          "BAR":"2020/08/22 12:33:10"
+        },
+        "case status":"active",
+        "plan":"OHVB"
+      },
+      {
+        "id":"OHVB17804",
+        "name":"Mauro Price",
+        "gender":"Male",
+        "age":"41",
+        "status history":{
+          "W":"2020/09/24 14:50:22",
+          "WAM":"2020/09/24 16:10:10",
+          "WPCF":"2020/09/26 10:23:11",
+          "BAT":"2020/09/26 16:10:10",
+          "BAR":"2020/09/27 10:23:11",
+          "AMC":"2020/10/22 13:51:12"
+        },
+        "case status":"inactive",
+        "plan":"OHVB"
+      },
+      {
+        "id":"OHVB17805",
+        "name":"Joseph C. Mahoney",
+        "gender":"Male",
+        "age":"45",
+        "status history":{
+          "W":"2020/09/24 14:53:12",
+          "WAM":"2020/09/24 16:52:20",
+          "WS":"2020/09/26 09:24:09",
+          "F":"2020/10/18 10:50:49"
+        },
+        "case status":"inactive",
+        "plan":"OHVB"
+      },
+      {
+        "id":"OHVB17806",
+        "name":"Lois A. Taylor",
+        "gender":"Female",
+        "age":"29",
+        "status history":{
+          "W":"2020/10/24 14:51:31",
+          "WAM":"2020/10/24 15:22:47",
+          "WPCF":"2020/10/25 09:07:36",
+          "WS":"2020/10/26 14:11:42",
+          "BAT":"2020/10/26 16:13:09"
+        },
+        "case status":"active",
+        "plan":"OHVB"
+      }
+    ];
+
 const useRowStyles = makeStyles({
   root: {
     '& > *': {
@@ -23,19 +113,35 @@ const useRowStyles = makeStyles({
   },
 });
 
-function createData(name, id, gender, age, caseStatus, plan) {
+function createData(name, id, gender, age, status, plan, statusHistory) {
   return {
     name,
     id,
     gender,
     age,
-    caseStatus,
+    status,
     plan,
-    history: [
-      { date: '2020-01-05', customerId: '11091700', amount: 3 },
-      { date: '2020-01-02', customerId: 'Anonymous', amount: 1 },
-    ],
+    history: handleHistory(statusHistory)
   };
+}
+
+class statusRecord {
+  constructor(status, time) {
+    this.status = status;
+    this.time = time;
+  }
+}
+
+function handleHistory(history) {
+  let recordArr = [];
+  for (let prop in history) {
+    let record = new statusRecord(prop,
+        new Date(history[prop]))
+    recordArr.push(record);
+  }
+  recordArr.sort((a,b) => (a.time < b.time) ? 1 : ((b.time < a.time) ? -1 : 0));
+  // recordArr.map(e => e.time.toLocaleString("en-US"));
+  return recordArr;
 }
 
 function Row(props) {
@@ -57,11 +163,12 @@ function Row(props) {
           <TableCell align="right">{row.id}</TableCell>
           <TableCell align="right">{row.gender}</TableCell>
           <TableCell align="right">{row.age}</TableCell>
-          <TableCell align="right">{row.caseStatus}</TableCell>
+          <TableCell align="right">{row.status}</TableCell>
+          <TableCell align="right">{row.plan}</TableCell>
 
         </TableRow>
         <TableRow>
-          <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={7}>
             <Collapse in={open} timeout="auto" unmountOnExit>
               <Box margin={1}>
                 <Typography variant="h6" gutterBottom component="div">
@@ -70,23 +177,17 @@ function Row(props) {
                 <Table size="small" aria-label="purchases">
                   <TableHead>
                     <TableRow>
-                      <TableCell>Date</TableCell>
-                      <TableCell>Customer</TableCell>
-                      <TableCell align="right">Amount</TableCell>
-                      <TableCell align="right">Total plan ($)</TableCell>
+                      <TableCell>Timestamp</TableCell>
+                      <TableCell align="right">Status</TableCell>
                     </TableRow>
                   </TableHead>
                   <TableBody>
                     {row.history.map((historyRow) => (
-                        <TableRow key={historyRow.date}>
+                        <TableRow key={historyRow.time}>
                           <TableCell component="th" scope="row">
-                            {historyRow.date}
+                            {historyRow.time.toLocaleString("en-US")}
                           </TableCell>
-                          <TableCell>{historyRow.customerId}</TableCell>
-                          <TableCell align="right">{historyRow.amount}</TableCell>
-                          <TableCell align="right">
-                            {Math.round(historyRow.amount * row.plan * 100) / 100}
-                          </TableCell>
+                          <TableCell align="right">{historyRow.status}</TableCell>
                         </TableRow>
                     ))}
                   </TableBody>
@@ -101,120 +202,38 @@ function Row(props) {
 
 Row.propTypes = {
   row: PropTypes.shape({
-    id: PropTypes.number.isRequired,
+    id: PropTypes.string.isRequired,
     age: PropTypes.number.isRequired,
-    gender: PropTypes.number.isRequired,
+    gender: PropTypes.string.isRequired,
     history: PropTypes.arrayOf(
         PropTypes.shape({
-          amount: PropTypes.number.isRequired,
-          customerId: PropTypes.string.isRequired,
-          date: PropTypes.string.isRequired,
+          status: PropTypes.string.isRequired,
+          time: PropTypes.instanceOf(Date).isRequired,
         }),
     ).isRequired,
     name: PropTypes.string.isRequired,
-    plan: PropTypes.number.isRequired,
-    caseStatus: PropTypes.number.isRequired,
+    plan: PropTypes.string.isRequired,
+    status: PropTypes.string.isRequired,
   }).isRequired,
 };
 
-// const rawData =
-//
-//       `{
-//         "id":"OHVB17801",
-//         "name":"Katlyn Merriman",
-//         "gender":"Female",
-//         "age":"33",
-//         "status history":{
-//           "W":"2020/01/24 14:50:22",
-//           "BAT":"2020/01/24 16:10:10",
-//           "BAR":"2020/01/26 10:23:11",
-//           "AMC":"2020/02/22 13:51:12"
-//         },
-//         "case status":"inactive",
-//         "plan":"OHVB"
-//       },
-//       {
-//         "id":"OHVB17802",
-//         "name":"Gabriel King",
-//         "gender":"Male",
-//         "age":"54",
-//         "status history":{
-//           "W":"2020/02/01 10:11:21",
-//           "BAT":"2020/02/01 15:02:58",
-//           "BAR":"2020/02/04 11:11:11",
-//           "DSS/G":"2020/02/15 09:20:22"
-//         },
-//         "case status":"inactive",
-//         "plan":"OHVB"
-//       },
-//       {
-//         "id":"OHVB17803",
-//         "name":"Patricia Devries",
-//         "gender":"Female",
-//         "age":"62",
-//         "status history":{
-//           "W":"2020/08/17 16:17:22",
-//           "BAT":"2020/08/19 15:45:22",
-//           "BAR":"2020/08/22 12:33:10"
-//         },
-//         "case status":"active",
-//         "plan":"OHVB"
-//       },
-//       {
-//         "id":"OHVB17804",
-//         "name":"Mauro Price",
-//         "gender":"Male",
-//         "age":"41",
-//         "status history":{
-//           "W":"2020/09/24 14:50:22",
-//           "WAM":"2020/09/24 16:10:10",
-//           "WPCF":"2020/09/26 10:23:11",
-//           "BAT":"2020/09/26 16:10:10",
-//           "BAR":"2020/09/27 10:23:11",
-//           "AMC":"2020/10/22 13:51:12"
-//         },
-//         "case status":"inactive",
-//         "plan":"OHVB"
-//       },
-//       {
-//         "id":"OHVB17805",
-//         "name":"Joseph C. Mahoney",
-//         "gender":"Male",
-//         "age":"45",
-//         "status history":{
-//           "W":"2020/09/24 14:53:12",
-//           "WAM":"2020/09/24 16:52:20",
-//           "WS":"2020/09/26 09:24:09",
-//           "F":"2020/10/18 10:50:49"
-//         },
-//         "case status":"inactive",
-//         "plan":"OHVB"
-//       },
-//       {
-//         "id":"OHVB17806",
-//         "name":"Lois A. Taylor",
-//         "gender":"Female",
-//         "age":"29",
-//         "status history":{
-//           "W":"2020/10/24 14:51:31",
-//           "WAM":"2020/10/24 15:22:47",
-//           "WPCF":"2020/10/25 09:07:36",
-//           "WS":"2020/10/26 14:11:42",
-//           "BAT":"2020/10/26 16:13:09"
-//         },
-//         "case status":"active",
-//         "plan":"OHVB"
-//       }`
-//
-// const data = JSON.parse(rawData);
+let rows = createRows(rawData);
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0, 3.99),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3, 4.99),
-  createData('Eclair', 262, 16.0, 24, 6.0, 3.79),
-  createData('Cupcake', 305, 3.7, 67, 4.3, 2.5),
-  createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5),
-];
+function createRows(rawData) {
+  let rows = []
+  for (let i = 0; i < rawData.length; i++) {
+    let row = createData(rawData[i].name,
+        rawData[i].id,
+        rawData[i].gender,
+        parseInt(rawData[i].age),
+        rawData[i]['case status'],
+        rawData[i].plan,
+        rawData[i]['status history']);
+    rows.push(row);
+  }
+  return rows;
+}
+console.log(rows);
 
 class Homepage extends Component {
   render() {
@@ -225,11 +244,12 @@ class Homepage extends Component {
             <TableHead>
               <TableRow>
                 <TableCell />
-                <TableCell>Dessert (100g serving)</TableCell>
+                <TableCell>Name</TableCell>
                 <TableCell align="right">id</TableCell>
-                <TableCell align="right">gender&nbsp;(g)</TableCell>
-                <TableCell align="right">age&nbsp;(g)</TableCell>
-                <TableCell align="right">caseStatus&nbsp;(g)</TableCell>
+                <TableCell align="right">Gender</TableCell>
+                <TableCell align="right">Age</TableCell>
+                <TableCell align="right">Case Status</TableCell>
+                <TableCell align="right">Plan</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
